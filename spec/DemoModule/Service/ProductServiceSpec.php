f3fd12cd1af85b2f3736e1d0aec8157c583fa867
David Contavalli <mauipipe@gmail.com>
2013-12-11 19:42:05 +0100

    product list created

--- spec/DemoModule/Service/ProductServiceSpec.php
+++ spec/DemoModule/Service/ProductServiceSpec.php
@@ -11,4 +11,30 @@ class ProductServiceSpec extends ObjectBehavior
     {
         $this->shouldHaveType('DemoModule\Service\ProductService');
     }
+    
+    function it_load_a_list_of_product() {
+        $expectedData = $this->loadMockData();
+        $this->getProducts()->shouldBeEqualTo($expectedData);
+    }
+
+    private function loadMockData() {
+        return array(
+            array(
+                "id" => 1,
+                "name" => "Borsa shopping bamboo in pelle fucsia",
+                "price" => 600
+            ),
+            array(
+                "id" => 2,
+                "name" => "Borsa shopping nice in vernice microguccissima",
+                "price" => 700
+            ),
+            array(
+                "id" => 3,
+                "name" => "Borsa a mani gucci nice in coccodrillo",
+                "price" => 13500
+            ),
+        );
+    }
+    
 }
--- src/DemoModule/Service/ProductService.php
+++ src/DemoModule/Service/ProductService.php
@@ -1,7 +1,19 @@
 <?php
 
 namespace DemoModule\Service;
+use DemoModule\Entity\Product;
 
 class ProductService
 {
+    const PRODUCT_PATH = "/../../../data/product.json";
+    private $products = array();
+    
+    public function getProducts()
+    {
+       $fileContent = file_get_contents(__DIR__ . self::PRODUCT_PATH);
+       $this->products = json_decode($fileContent, true);
+       
+       return $this->products;
+    }
+      
 }
