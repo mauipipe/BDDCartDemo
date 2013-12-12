d2b5d2d2d0fbcca8a97e9665c6f78bf5b6ece63d
David Contavalli <mauipipe@gmail.com>
2013-12-12 13:06:41 +0100

    process cart added

--- spec/BDDCartDemo/CartSpec.php
+++ spec/BDDCartDemo/CartSpec.php
@@ -15,10 +15,7 @@ class CartSpec extends ObjectBehavior
     }
     
     function it_should_add(Product $product){
-        
-        $product->setName("Product");
-        $product->setPrice(600);
-        
+           
         $this->add($product);
         $this->getQty()->shouldReturn(1);
         
@@ -26,20 +23,45 @@ class CartSpec extends ObjectBehavior
     
     function it_should_produce_a_list_of(Product $product){
         
-        $product->setName("Product");
-        $product->setPrice(600);
-        $product->setId(1);
-        
+        $mockId = 1;
+        $product->getId()->willReturn($mockId);
         $this->add($product);
-        
-        $this->getProducts()->shouldBeEqualTo(array($product));
+        $this->getProducts()->shouldBeEqualTo(array($mockId => $product));
         
     }
     
     function it_should_remove(Product $product){
         
+        $mockId = 1;
+        $product->getId()->willReturn($mockId);
         $this->add($product);
-        $this->remove(0);
+        $this->remove($mockId);
         $this->getQty()->shouldReturn(0);
+    }
+    
+    
+    function it_process_an_order(Product $product){
+        
+        $mockProducts = array(
+            array(
+                "id"=>1,
+                "price"=>600
+            ),
+            array(
+                "id"=>2,
+                "price"=>700
+            )
+        );
+        
+        
+        foreach($mockProducts as $mockProduct) {
+            
+            $product->getId()->willReturn($mockProduct['id']);
+            $product->getPrice()->willReturn($mockProduct['price']);
+            $this->add($product);
+            
+        }
+        
+        $this->process()->shouldReturn(true);
     }
 }
--- src/BDDCartDemo/Cart.php
+++ src/BDDCartDemo/Cart.php
@@ -2,32 +2,81 @@
 
 namespace BDDCartDemo;
 use BDDCartDemo\Entity\Product;
+use BDDCartDemo\Manager\FileManager;
 
 class Cart
 {
     private $products;
-    
+      
     public function __construct(){
         $this->products = array();
     }
-    
+    /**
+     * 
+     * @param \BDDCartDemo\Entity\Product $product
+     */
     public function add(Product $product)
     {
         $this->products[$product->getId()] = $product;
     }
-
+    
+    /**
+     * 
+     * @return int
+     */
     public function getQty()
     {
         return sizeof($this->products);
     }
-
+    
+    /**
+     * 
+     * @return array 
+     */
     public function getProducts()
     {
         return $this->products;
     }
-
+    
+    /**
+     * 
+     * @param int $key
+     */
     public function remove($key)
     {
         unset($this->products[$key]);
+    }
+    
+    /**
+     * 
+     * @return boolean
+     */
+    public function process()
+    {
+       $fileManager = new FileManager();
+       $path = __DIR__ . "/../../data/order.json";
+       $fileManager->setPath($path);
+       $content = $fileManager->read();
+       
+       $message = "Pagamento avvenuto con successo";
+       $total = 0;
+       
+       foreach ($this->products as $product){
+           $total += $product->getPrice();
+       }
+       
+       $orderNr = sizeof($content);
+       
+       $order = array(
+           "ord_nr"=>$orderNr,
+           "total"=>$total
+               );
+       
+       if($fileManager->save($order)) {
+         return true;
+       }
+       
+       return false;
+       
     }
 }
